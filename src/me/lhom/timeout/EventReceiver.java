f44e233d7a34a6ff76a48c95685ba315d9e26bd1
robux4 <robux4@gmail.com>
2011-03-10 19:05:09 +0100

    the power plug needs to be handled too

--- src/me/lhom/timeout/EventReceiver.java
+++ src/me/lhom/timeout/EventReceiver.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
+import android.os.PowerManager;
 import android.text.format.DateUtils;
 import android.text.format.Time;
 import android.util.Log;
@@ -21,26 +22,19 @@ public class EventReceiver extends BroadcastReceiver {
 		if (action!=null) {
 			if (Intent.ACTION_SCREEN_OFF.equals(action)) {
 				// start a timer to disable Wifi
-				Intent intentService = new Intent(context, getClass());
-				intentService.setAction(ACTION_TIMEOUT);
-				PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intentService, 0);
-				AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-
-				Time time = new Time();
-				time.set(System.currentTimeMillis() + 10 * DateUtils.SECOND_IN_MILLIS);
-				long nextStart = time.toMillis(false);
-				alarmManager.set(0, nextStart, pendingIntent);
+				setPendingAlarm(context);
 			}
 			else if (Intent.ACTION_SCREEN_ON.equals(action)) {
 				// cancel the timer to disable Wifi
-				Intent intentService = new Intent(context, getClass());
-				intentService.setAction(ACTION_TIMEOUT);
-				PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intentService, 0);
-				AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-				alarmManager.cancel(pendingIntent);
-				
 				// TODO: enable back wifi only it is was enabled before we disabled it
+				setPendingAlarm(context);
 				setWifiState(context, true);
+			}
+			else if (Intent.ACTION_POWER_DISCONNECTED.equals(action)) {
+				setPendingAlarm(context);
+			}
+			else if (Intent.ACTION_POWER_CONNECTED.equals(action)) {
+				setPendingAlarm(context);
 			}
 			else if (ACTION_TIMEOUT.equals(action)) {
 				setWifiState(context, false);
@@ -48,9 +42,35 @@ public class EventReceiver extends BroadcastReceiver {
 		}
 	}
 
+	private static void setPendingAlarm(Context context) {
+		// TODO: get the screen state
+		PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+		boolean screenIsOn = pm.isScreenOn();
+		
+		// TODO: get the power connection state
+		WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+		boolean wifiIsOn = wifiManager.isWifiEnabled();
+		
+		Log.w("Timeout", "screenIsOn:"+screenIsOn+" wifiIsOn:"+wifiIsOn);
+
+		Intent intentService = new Intent(context, EventReceiver.class);
+		intentService.setAction(ACTION_TIMEOUT);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intentService, 0);
+		AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+
+		if (screenIsOn)
+			alarmManager.cancel(pendingIntent);
+		else {
+			Time time = new Time();
+			time.set(System.currentTimeMillis() + 10 * DateUtils.SECOND_IN_MILLIS);
+			long nextStart = time.toMillis(false);
+			alarmManager.set(0, nextStart, pendingIntent);
+			setPendingAlarm(context);
+		}
+	}
 	
 	private static void setWifiState(Context context, boolean enabled) {
-		Log.w("Timeout", "We should disable WIFI here");
+		Log.e("Timeout", "We should disable WIFI here");
 		
 		// TODO: only disable if power not plugged and screen is off
 		
