d3e2ce789840f153d7eba853b8dc41d2441e7db6
robux4 <robux4@gmail.com>
2011-03-10 19:05:45 +0100

    get the plugged state on startup and keep track of its changes

--- src/me/lhom/timeout/Configure.java
+++ src/me/lhom/timeout/Configure.java
@@ -1,8 +1,6 @@
 package me.lhom.timeout;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class Configure extends Activity {
@@ -11,11 +9,5 @@ public class Configure extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        IntentFilter screenOn = new IntentFilter(Intent.ACTION_SCREEN_ON);
-        IntentFilter screenOff = new IntentFilter(Intent.ACTION_SCREEN_OFF);
-        EventReceiver mReceiver = new EventReceiver();
-        registerReceiver(mReceiver, screenOn);
-        registerReceiver(mReceiver, screenOff);
     }
 }
--- src/me/lhom/timeout/EventReceiver.java
+++ src/me/lhom/timeout/EventReceiver.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
+import android.os.BatteryManager;
 import android.os.PowerManager;
 import android.text.format.DateUtils;
 import android.text.format.Time;
@@ -14,6 +15,7 @@ import android.util.Log;
 public class EventReceiver extends BroadcastReceiver {
 
 	private static final String ACTION_TIMEOUT = "me.lhom.timeout.action.TIMEOUT";
+	private static boolean isPowerPlugged = false;
 	
 	@Override
 	public void onReceive(Context context, Intent intent) {
@@ -30,47 +32,48 @@ public class EventReceiver extends BroadcastReceiver {
 				setPendingAlarm(context);
 				setWifiState(context, true);
 			}
-			else if (Intent.ACTION_POWER_DISCONNECTED.equals(action)) {
-				setPendingAlarm(context);
-			}
-			else if (Intent.ACTION_POWER_CONNECTED.equals(action)) {
+			else if (Intent.ACTION_BATTERY_CHANGED.equals(action)) {
+				isPowerPlugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0)!=0;
 				setPendingAlarm(context);
 			}
 			else if (ACTION_TIMEOUT.equals(action)) {
 				setWifiState(context, false);
 			}
+			else if (WifiManager.WIFI_STATE_CHANGED_ACTION.equals(action)) {
+				Log.i("Timeout", "wifi changed "+intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN));
+				//TODO: if disabled, cancel the possible alarm
+			}
 		}
 	}
 
 	private static void setPendingAlarm(Context context) {
-		// TODO: get the screen state
+		// get the screen state
 		PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
 		boolean screenIsOn = pm.isScreenOn();
 		
-		// TODO: get the power connection state
+		// get the wifi state
 		WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
 		boolean wifiIsOn = wifiManager.isWifiEnabled();
 		
-		Log.w("Timeout", "screenIsOn:"+screenIsOn+" wifiIsOn:"+wifiIsOn);
+		Log.w("Timeout", "screenIsOn:"+screenIsOn+" wifiIsOn:"+wifiIsOn+" isPlugged:"+isPowerPlugged);
 
 		Intent intentService = new Intent(context, EventReceiver.class);
 		intentService.setAction(ACTION_TIMEOUT);
 		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intentService, 0);
 		AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
 
-		if (screenIsOn)
+		if (screenIsOn || isPowerPlugged)
 			alarmManager.cancel(pendingIntent);
 		else {
 			Time time = new Time();
-			time.set(System.currentTimeMillis() + 10 * DateUtils.SECOND_IN_MILLIS);
+			time.set(System.currentTimeMillis() + 10 * DateUtils.SECOND_IN_MILLIS); //TODO: make the 10s a parameter
 			long nextStart = time.toMillis(false);
 			alarmManager.set(0, nextStart, pendingIntent);
-			setPendingAlarm(context);
 		}
 	}
 	
 	private static void setWifiState(Context context, boolean enabled) {
-		Log.e("Timeout", "We should disable WIFI here");
+		Log.e("Timeout", "We should enable WIFI "+enabled);
 		
 		// TODO: only disable if power not plugged and screen is off
 		
